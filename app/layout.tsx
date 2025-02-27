209fb2320b2398ee1f122555693d71bf61212f95
therealemjy <maxime.julian.creations@gmail.com>
2025-02-27 12:55:12 +0100

    feat: add form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { WagmiProvider, createConfig } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { zksync } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { WALLET_CONNECT_PROJECT_ID } from "./constants";
@@ -9,44 +9,44 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 const config = createConfig(
-	getDefaultConfig({
-		// Your dApps chains
-		chains: [mainnet],
-		// Required API Keys
-		walletConnectProjectId: WALLET_CONNECT_PROJECT_ID,
-		// Required App Info
-		appName: "Venus Protocol",
-	}),
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [zksync],
+    // Required API Keys
+    walletConnectProjectId: WALLET_CONNECT_PROJECT_ID,
+    // Required App Info
+    appName: "Venus Protocol",
+  }),
 );
 
 const queryClient = new QueryClient();
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<WagmiProvider config={config}>
-					<QueryClientProvider client={queryClient}>
-						<ConnectKitProvider>{children}</ConnectKitProvider>
-					</QueryClientProvider>
-				</WagmiProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+      >
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <ConnectKitProvider>{children}</ConnectKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,37 +2,67 @@
 
 import { useState } from "react";
 import { ConnectKitButton } from "connectkit";
-import { useAccount } from "wagmi";
+import { useAccount, useWriteContract } from "wagmi";
+import { abi } from "./safeWalletAbi";
+import { SAFE_WALLET_CONTRACT_ADDRESS } from "./constants";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-	const [hash, setHash] = useState<string>("");
-	const { isConnected } = useAccount();
+  const [hash, setHash] = useState<string>("");
+  const [transactionSentHash, setTransactionSentHash] = useState<string>("");
+  const { isConnected } = useAccount();
+  const { writeContractAsync, isPending } = useWriteContract();
 
-	const handleSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
-	};
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-	return (
-		<div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
-			<main className="flex flex-col gap-4 items-center sm:items-start">
-				{isConnected ? (
-					<form onSubmit={handleSubmit}>
-						<input
-							className="block w-full"
-							name="hash"
-							value={hash}
-							onChange={(e) => setHash(e.currentTarget.value)}
-							placeholder="Enter hash here"
-						/>
+    try {
+      const transactionHash = await writeContractAsync({
+        address: SAFE_WALLET_CONTRACT_ADDRESS,
+        abi,
+        functionName: "approveHash",
+        args: [hash as `0x${string}`],
+      });
 
-						<button className="w-full" type="submit">
-							Approve
-						</button>
-					</form>
-				) : (
-					<ConnectKitButton />
-				)}
-			</main>
-		</div>
-	);
+      setTransactionSentHash(transactionHash);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
+      <main className="flex flex-col gap-3 items-center sm:items-start w-full max-w-[500px]">
+        <ConnectKitButton />
+
+        {isConnected && (
+          <>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-3 w-full"
+            >
+              <Input
+                className="block w-full"
+                name="hash"
+                value={hash}
+                onChange={(e) => setHash(e.currentTarget.value)}
+                placeholder="Enter hash here"
+              />
+
+              <Button className="w-full" type="submit" disabled={isPending}>
+                {isPending ? "Sending..." : "Approve"}
+              </Button>
+            </form>
+
+            {!!transactionSentHash && (
+              <p className="text-green-500 bold text-center">
+                Transaction sent: {transactionSentHash}
+              </p>
+            )}
+          </>
+        )}
+      </main>
+    </div>
+  );
 }
